4d671f7a40958c0deed183d494c9df6d756690d2
Luis Rodriguez <luisu_10@hotmail.com>
2022-08-17 17:06:15 -0500

    Fixed pagination bug

--- src/Books.js
+++ src/Books.js
@@ -1,79 +1,87 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import StarRatingDisplay from "./StarRatingDisplay";
-import Pagination from './Pagination';
+import Pagination from "./Pagination";
 
 class Books extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
       currPage: 1,
       booksPerPage: 6,
-    }
+    };
     this.setCurrentPage = this.setCurrentPage.bind(this);
   }
 
-  setCurrentPage(curPage) {
-    this.setState({ currPage: curPage });
+  setCurrentPage(currPage) {
+    this.setState({ currPage: currPage });
   }
 
   render() {
-    const { books, auth } = this.props;
     const pageNumber = this.props.match.params.id * 1;
+    const { books, auth } = this.props;
     const { setCurrentPage } = this;
-    const idxOfLastRecord = this.state.currPage * this.state.booksPerPage;
+    let idxOfLastRecord;
+    if (!pageNumber) {
+      idxOfLastRecord = this.state.currPage * this.state.booksPerPage;
+    } else {
+      idxOfLastRecord = pageNumber * this.state.booksPerPage;
+    }
+
     const idxOfFirstRecord = idxOfLastRecord - this.state.booksPerPage;
     const listBooks = books.slice(idxOfFirstRecord, idxOfLastRecord);
     const numPages = Math.ceil(books.length / this.state.booksPerPage);
 
     return (
-      <div id='books-page'>
-        {
-          auth.id ? <h2 className='user-name'>
-          Welcome, {auth.firstName} {auth.lastName}!
-        </h2> : null
-        }
+      <div id="books-page">
+        {auth.id ? (
+          <h2 className="user-name">
+            Welcome, {auth.firstName} {auth.lastName}!
+          </h2>
+        ) : null}
         <h2>Books</h2>
-        <div id='books'>
-          {
-            listBooks.map(book => {
-              return (
-                <div key={book.id} className='book-info'>
-                  <img
-                    className="photo-books"
-                    src={book.imageUrl}>
-                  </img>
-                  <h3>
-                    <Link to={`/books/${book.id}`}>{book.title}</Link>
-                  </h3>
-                  <p><span>Author:</span> {book.publisher}</p>
-                  <p><span>Year:</span>  { new Date(book.year).getFullYear()}</p>
-                  <p><span>Price:</span>  $ {book.price}</p>
-                  <p><span>Stock:</span>  {book.inventory}</p>
-                  <StarRatingDisplay  book={book} /> 
-                  <button>Add to Cart</button>
-                </div>
-              )
-            })
-          }
+        <div id="books">
+          {listBooks.map((book) => {
+            return (
+              <div key={book.id} className="book-info">
+                <img className="photo-books" src={book.imageUrl}></img>
+                <h3>
+                  <Link to={`/books/${book.id}`}>{book.title}</Link>
+                </h3>
+                <p>
+                  <span>Author:</span> {book.publisher}
+                </p>
+                <p>
+                  <span>Year:</span> {new Date(book.year).getFullYear()}
+                </p>
+                <p>
+                  <span>Price:</span> $ {book.price}
+                </p>
+                <p>
+                  <span>Stock:</span> {book.inventory}
+                </p>
+                <StarRatingDisplay  book={book} /> 
+                <button>Add to Cart</button>
+              </div>
+            );
+          })}
         </div>
         <Pagination
           numPages={numPages}
           setCurrentPage={setCurrentPage}
           pageNumber={pageNumber}
-          flag='books'
+          flag="books"
         />
       </div>
     );
   }
 }
 
-const mapStateToProps = ({books, auth}) => {
+const mapStateToProps = ({ books, auth }) => {
   return {
     books,
-    auth
+    auth,
   };
 };
 
