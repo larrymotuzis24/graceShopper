f8f0349ca05c66c28946afa690e8dd1b5b288c1d
Luis Rodriguez <luisu_10@hotmail.com>
2022-08-20 23:41:42 -0500

    Added Product Category table

--- db/index.js
+++ db/index.js
@@ -5,19 +5,23 @@ const User = require('./User');
 const LineItem = require('./LineItem');
 const Order = require('./Order');
 const State = require('./State');
+const ProductCategory = require('./ProductCategory');
 const seedUsers = require('./seed-users.json');
 const seedProducts = require('./seed-products.json');
 const seedOrders = require('./seed-orders.json');
 const seedLineItems = require('./seed-lineItems.json');
 const seedStates = require('./seed-states.json');
+const seedProductCategory = require('./seed-productCategory.json')
 
 User.hasMany(Order);
 Order.hasMany(LineItem);
 LineItem.belongsTo(Product);
+Product.belongsTo(ProductCategory);
 
 const seeder = async() =>{
   await conn.sync({force: true});
   await Promise.all(seedUsers.map(user => User.create(user)));
+  await Promise.all(seedProductCategory.map(category => ProductCategory.create(category)))
   await Promise.all(seedProducts.map(product => Product.create(product)));
   await Promise.all(seedOrders.map(order => Order.create(order)));
   await Promise.all(seedLineItems.map(lineItem => LineItem.create(lineItem)));
