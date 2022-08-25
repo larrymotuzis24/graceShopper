ecd8a975fd284d477954211bdbcb475393ced067
ioan butiu <36743419+ioanbutiu@users.noreply.github.com>
2022-08-25 18:11:18 -0400

    homepage div fix

--- src/Home.js
+++ src/Home.js
@@ -11,22 +11,23 @@ import StarRatingDisplay from './StarRatingDisplay';
 const Home = ({ books, auth }) => {
   const topBooks = books.filter((book) => (book.rating = 5));
   return (
-
     <div className="container" id="home-page">
-
       <FeaturedCarousel />
       <h2 className="display-4 my-4">Top Rated Books</h2>
       <div className="row row-cols-2 row-cols-lg-4">
         {topBooks.map((book) => {
           return (
             <div key={book.id} className="col">
-              <div className="card border-0 bg-transparent mb-5" style={{background: '#eef7ea'}}>
-                  <Link
-                    id={book.id}
-                    to={`books/${book.id}`}
-                    className="text-decoration-none"
-                  >
-                  <div style={{height: '19vh'}}>
+              <div
+                className="card border-0 bg-transparent mb-5"
+                style={{ background: '#eef7ea' }}
+              >
+                <Link
+                  id={book.id}
+                  to={`books/${book.id}`}
+                  className="text-decoration-none"
+                >
+                  <div style={{ height: '19vh' }}>
                     <img
                       src={book.imageUrl}
                       style={{
@@ -38,7 +39,10 @@ const Home = ({ books, auth }) => {
                       className="rounded-2 mb-2 card-img-top"
                     />
                   </div>
-                  <div className="card-body p-0" style={{background: '#eef7ea'}}>
+                  <div
+                    className="card-body p-0"
+                    style={{ background: '#eef7ea' }}
+                  >
                     <p className="my-0 text-black">{book.title}</p>
                     <p className="my-0 text-black">{book.author}</p>
                     <p className="my-0 text-black">${book.price}</p>
@@ -46,29 +50,62 @@ const Home = ({ books, auth }) => {
                 </Link>
               </div>
             </div>
-          );``
+          );
+          ``;
         })}
       </div>
-      <div style={{display: 'flex', height: '600px', width: '35vw', borderRadius: '25px'}}>
-        <div style={{display: 'inline-block', height: '100%', width: '100%', float: 'right'}}>
-          <div style={{
-          height: '100%',
-          width: '100%',
-          borderRadius: '25px 0px 0px 25px',
-          background: '#0F301D'
-          }}>
-            <h4 style={{textIndent: '15px', color: 'pink'}}> Code Source Books was developed by Luis, Larry, Seth, and Ioan. </h4>
-            <h4 style={{textIndent: '15px', color: 'pink'}}> This is the nicest bookstore website you've ever seen!</h4>
+      <div
+        style={{
+          display: 'flex',
+          height: '600px',
+          width: '35vw',
+          borderRadius: '25px',
+        }}
+      >
+        <div
+          style={{
+            display: 'inline-block',
+            height: '100%',
+            width: '100%',
+            float: 'right',
+          }}
+        >
+          <div
+            style={{
+              height: '100%',
+              width: '100%',
+              borderRadius: '25px 0px 0px 25px',
+              background: '#0F301D',
+            }}
+          >
+            <h4 style={{ textIndent: '15px', color: 'pink' }}>
+              {' '}
+              Code Source Books was developed by Luis, Larry, Seth, and Ioan.{' '}
+            </h4>
+            <h4 style={{ textIndent: '15px', color: 'pink' }}>
+              {' '}
+              This is the nicest bookstore website you've ever seen!
+            </h4>
           </div>
         </div>
-        <div style={{display: 'inline-block', height: '100%', width: '100%', float: 'left'}}>
-          <img style={{
-          height: '100%',
-          width: '100%',
-          borderRadius: '0 25px 25px 0'}}src='https://images.pexels.com/photos/1029807/pexels-photo-1029807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'/>
+        <div
+          style={{
+            display: 'inline-block',
+            height: '100%',
+            width: '100%',
+            float: 'left',
+          }}
+        >
+          <img
+            style={{
+              height: '100%',
+              width: '100%',
+              borderRadius: '0 25px 25px 0',
+            }}
+            src="https://images.pexels.com/photos/1029807/pexels-photo-1029807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          />
         </div>
       </div>
-    </div>
     </div>
   );
 };
